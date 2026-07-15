837bfbd1d3f78889375d29fd0e4f98549027b2a8
Aman Maurya <amanmauryaanu76@gmail.com>
2026-07-15 15:53:31 +0530

    Initialize Hardhat configuration file
    Set up Hardhat configuration with Solidity version and network settings.

---
+++ hardhat.js
@@ -0,0 +1,28 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 1337,
+    },
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      chainId: 43113,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+};
